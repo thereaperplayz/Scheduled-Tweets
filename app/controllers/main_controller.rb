5e70f5125dc885d9383fe2a364967cbf97a2aa28
thereaperplayz <micahcriddle@gmail.com>
2023-07-14 21:51:28 -0600

    just to update my recent changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,6 @@
 class MainController < ApplicationController
   def index
-    # Your index action code here
-  end
-
-  def add_index
-    puts "add_index action called"
-    flash.now[:notice] = "Logged in successfully"
-    flash.now[:alert] = "Invalid email or password"
-    render "index"
+    flash[:notice] = "Logged in successfully"
+    flash[:alert] = "Invalid email or password"
   end
 end
