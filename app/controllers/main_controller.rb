7d077c506e8d0c2c6d8344f590da5f80940cb935
thereaperplayz <micahcriddle@gmail.com>
2023-07-16 03:05:17 -0600

    to update github so I can share this code and look for solutions

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,10 @@
 class MainController < ApplicationController
   def index
-    flash[:notice] = "Logged in successfully"
-    flash[:alert] = "Invalid email or password"
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+      flash.now[:notice] = "Logged in successfully"
+    else
+      flash.now[:alert] = "Invalid email or password"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # GET /about
   get "about-us", to: "about#index", as: :about
 
-  root to: "main#index"
+  root to: 'main#index'
+  get '/sign_up', to: 'registrations#new', as: 'sign_up'
+  post "users", to: "registrations#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
