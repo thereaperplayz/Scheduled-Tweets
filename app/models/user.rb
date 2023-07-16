1a52116c136733b69cefa5e311bd977e72e85b3d
thereaperplayz <micahcriddle@gmail.com>
2023-07-16 17:04:29 -0600

    to update the hub with the most current code

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@
 #Password_confirmationLstring virtual
 class User < ApplicationRecord
     has_secure_password
-
-    validates :email, presence: true, format: { with: /\A^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
-    
-end
+  
+    validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "must be a valid email address" }
+  end
+  
