bbc495c709db6eb426de11834b4b7e4fbb5bc3f9
Micah Criddle <35113922+thereaperplayz@users.noreply.github.com>
2023-07-20 01:07:57 +0000

    to update my code with the correct way to display error messages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,22 +1,23 @@
 class RegistrationsController < ApplicationController
-    def new
-        @user = User.new
-        @user.valid?  # Run validations to populate error messages
-    end           
+  def new
+    @user = User.new
+    # @user.valid?  # Run validations to populate error messages
+  end           
 
-    def create 
-        @user = User.new(user_params)
-        if @user.save
-          session[:user_id] = @user.id
-          redirect_to root_path, notice: "Successfully created account!"
-        else
-          render plain: "user creation failed"
-        end          
-      end     
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Successfully created account!"
+    else
+      #flash.now[:alert] = @user.errors.full_messages.join(", ")
+      render :new
+    end          
+  end     
 
-    private 
+  private 
 
-    def user_params 
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+  def user_params 
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@
 #password:string virtual
 #Password_confirmationLstring virtual
 class User < ApplicationRecord
-    has_secure_password
-  
-    validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "must be a valid email address" }
-  end
-  
+  has_secure_password
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "must be a valid email address" }
+end
+
